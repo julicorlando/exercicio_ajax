9ae23817e8276faec756314e0bfaa8baae8a1706
Julio <falecomjulioorlando@outlook.com>
2026-03-11 16:17:46 -0300

    Adicionando exercicio TypeScript

---
+++ exercicio_ts/index.ts
@@ -0,0 +1,13 @@
+// função de multiplicação
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
+
+// função de saudação
+function saudacao(nome: string): string {
+  return "Olá " + nome;
+}
+
+// testes
+console.log(multiplicar(5, 4));
+console.log(saudacao("João"));
